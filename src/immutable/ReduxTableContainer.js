37154d31a1bf9bcd88c5d6d6708f39458440ca5e
Grant Forrest <gforrest@bandwidth.com>
2017-04-18 11:15:38 -0400

    fix another mistake

--- src/immutable/ReduxTableContainer.js
+++ src/immutable/ReduxTableContainer.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import * as actions from './actions';
+import * as actions from '../actions';
 import ReduxTable from '../ReduxTable';
 
 const mapStateToProps = (state, ownProps) => state.get('tables')[ownProps.tableName] ? {
